4738f64a753edbdebb2d49c6304f9f1971d0d86b
seejayyy <cjhuskerz@gmail.com>
2023-12-12 09:30:04 +0000

    Added creating sales order feature for chatbot

--- assistbot/assistbot/doctype/assistbot_chat/assistbot_chat.py
+++ assistbot/assistbot/doctype/assistbot_chat/assistbot_chat.py
@@ -8,6 +8,7 @@ from frappe.model.document import Document
 from frappe import _
 from frappe.integrations.utils import make_post_request
 import re
+import datetime
 
 class AssistBotChat(Document):
 	pass
@@ -21,6 +22,7 @@ def execute_instruction(doc):
 	if "Create" in operation[0]:
 		# create 
 		create_entry(context, info)
+		frappe.msgprint(_("{0} is created").format(context[0]))
 	elif "Delete" in operation[0]:
 		# delete 
 		delete_entry(context, info)
@@ -36,7 +38,7 @@ def create_entry(context, info):
 	# customer, company, item, quantity (defaulted by the item), source warehouse
 	info = re.split('\s', info)
 	info = list(filter(None,info))
-	print(info)
+	# print(info)
 	# check if instruction specify customer
 	try:
 		customer_index = info.index("Customer")
@@ -57,28 +59,42 @@ def create_entry(context, info):
 
 	# check if info is valid
 	try:
-		customer = info[customer_index+1:company_index]
-		customer = ' '.join(customer)
-		customer_data = get_data_from_doctype("Customer","customer_name", customer)
+		customer_info = info[customer_index+1:company_index]
+		customer_info = ' '.join(customer_info)
+		customer_data = get_data_from_doctype("Customer","customer_name", customer_info)
+		print(customer_data)
 		if len(customer_data) == 0:
 			frappe.throw(_("Invalid customer"))
+		if len(customer_data) > 1:
+			frappe.throw(_("Multiple records detected. Please specify the correct customer."))
+		customer_queried = customer_data[0].name
 	except:
 		frappe.throw(_("Invalid customer"))
 	try:
-		company = info[company_index+1:item_index]
-		company = ' '.join(company)
-		company_data = get_data_from_doctype("Company","company_name", company)
+		company_info = info[company_index+1:item_index]
+		company_info = ' '.join(company_info)
+		company_data = get_data_from_doctype("Company","company_name", company_info)
+		print(company_data)
 		if len(company_data) == 0:
 			frappe.throw(_("Invalid company"))
+		if len(company_data) > 1:
+			frappe.throw(_("Multiple records detected. Please specify the correct company."))
+		company_queried = company_data[0].name
 	except:
 		frappe.throw(_("Invalid company"))
 	
 	try:
-		item = info[item_index+1:warehouse_index]
-		item = ' '.join(item)
-		item_data = get_data_from_doctype("Item","item_name", item)
+		print("get item")
+		item_info = info[item_index+1:warehouse_index]
+		item_info = ' '.join(item_info)
+		item_data = get_data_from_doctype("Item","item_name", item_info)
+		print(item_data)
 		if len(item_data) == 0:
 			frappe.throw(_("Invalid item"))
+		if len(item_data) > 1:
+			frappe.throw(_("Multiple records detected. Please specify the correct item."))
+		item_queried = item_data[0].name
+		item_valuation_rate = item_data[0].valuation_rate
 	except:
 		frappe.throw(_("Invalid item"))
 	
@@ -86,12 +102,40 @@ def create_entry(context, info):
 		warehouse = info[warehouse_index+1:]
 		warehouse = ' '.join(warehouse)
 		warehouse_data = get_data_from_doctype("Warehouse","warehouse_name", warehouse)
+		print(warehouse_data)
 		if len(warehouse_data) == 0:
 			frappe.throw(_("Invalid warehouse"))
+		if len(warehouse_data) > 1:
+			frappe.throw(_("Multiple records detected. Please specify the correct warehouse."))
+		warehouse_queried = warehouse_data[0].name
 	except:
 		frappe.throw(_("Invalid warehouse"))
 
-	return
+	# param = json.dumps({
+	# 	"doctype": context[0].title(),
+	# 	"customer": customer_queried,
+	# 	"company": company_queried,
+	# 	"item": item_queried,
+	# 	"warehouse": warehouse_queried
+	# })
+	sales_order_doc = frappe.get_doc({
+		"doctype": "Sales Order",
+		"company": company_queried,
+		"customer": customer_queried,
+		"set_warehouse": warehouse_queried,
+		"delivery_date": datetime.date.today(),
+		"items": [
+			{
+				"item_code": item_queried,
+				"qty": 1.0,
+				"rate": item_valuation_rate,
+				"warehouse": warehouse_queried
+			}
+		]
+	})
+	sales_order_doc.insert()
+	# webhook_post(param)
+	return 
 
 def delete_entry(context, info):
 	return
@@ -100,20 +144,20 @@ def update_entry(context, info):
 	return
 
 def list_entry(context, info):
-	db_data = get_data_from_doctype(context, info)
-	# add a check to see if item exists
-	if len(db_data) == 0:
-		frappe.msgprint(_("Item does not exst"))
-	else:
-		output = json.dumps(db_data[0])
-		frappe.msgprint(_(output))
+	# db_data = get_data_from_doctype(context, info)
+	# # add a check to see if item exists
+	# if len(db_data) == 0:
+	# 	frappe.msgprint(_("Item does not exst"))
+	# else:
+	# 	output = json.dumps(db_data[0])
+	# 	frappe.msgprint(_(output))
 	return
 
 def get_data_from_doctype(doctype, field, info):
 
-	print(doctype)
-	print(field)
-	print(info)
+	# print(doctype)
+	# print(field)
+	# print(info)
 	
 	# most operations do things towards items
 	# frappe.db.get_list(doctype.title(), fields="*", filters={field: info[0]}) 
@@ -130,7 +174,7 @@ def extract_context(instruction):
 	response = execute(prompt)
 
 	splitted = re.split(':\s|,|=|-\s|[\n]', response)
-	print(splitted)
+	# print(splitted)
 	context_index = splitted.index("Context")	
 	operation_index = splitted.index("Operation")
 	information_index = splitted.index("Information")
@@ -141,9 +185,9 @@ def extract_context(instruction):
 
 	information = ' '.join(information_list)
 
-	print(context)
-	print(operation)
-	print(information)
+	# print(context)
+	# print(operation)
+	# print(information)
 	return context, operation, information
 
 def execute(instruction):
@@ -165,3 +209,10 @@ def execute(instruction):
 	response = make_post_request(url, headers=headers, data=data)
 	frappe.msgprint(_(response["choices"][0]["message"]["content"]))
 	return response["choices"][0]["message"]["content"]
+
+def webhook_post(param):
+	url = 'http://192.168.4.223:5678/webhook-test/erpnextchatbot'
+	headers = {
+		'Content-type': 'application/json;'
+	}
+	response = make_post_request(url, headers=headers, data=param)
